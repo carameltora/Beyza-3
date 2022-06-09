f0df43c48bdd491e06baf25cc30dda43e5e1e2fc
Arquidex ~ Arki <88553657+carameltora@users.noreply.github.com>
2022-06-09 16:52:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,14 +3,33 @@ let Username = "n"; let Password = "p"; let Server = "s";
 <!--initialize the params-->
 
 function Login1() {
-  // Build formData object.
-let formData = new FormData();
-formData.append('name', 'John');
-formData.append('password', 'John123');
+  <!--initialize the params-->
+  Username = document.getElementById('usernameinput').value;
+  Password = document.getElementById('passwordinput').value;
+  Server = document.getElementById('serverinput').value;
 
-fetch("https://youtube.com",
-    {
+  <!--Build the form data-->
+  let formData = new FormData();
+  formData.append('client_id', 'unity.client');
+  formData.append('client_secret', 'secret');
+  formData.append('grant_type', 'password');
+  formData.append('scope', 'openid nebula offline_access');
+  formData.append('username', Server + '|' + Username);
+  formData.append('password', Password);
+  formData.append('acr_values', 'gameId:j68d');
+
+  <!--Getting the url-->
+  let url = "https://eu.mspapis.com/loginidentity/connect/token";
+
+  if(Server === 'us' || Server === 'UnitedStates') {
+    url = "https://us.mspapis.com/loginidentity/connect/token":
+  }
+
+  <!--Send first login request to api-->
+  fetch(url,
+  {
         body: formData,
         method: "post"
-    });
+  });
+
 }
