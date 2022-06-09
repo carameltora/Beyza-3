92651ea34135d556f65cb3a3bb768989f9c37d6d
Arquidex ~ Arki <88553657+carameltora@users.noreply.github.com>
2022-06-09 16:57:10 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 
 let Username = "n"; let Password = "p"; let Server = "s";
-<!--initialize the params-->
+
 
 function Login1() {
-  <!--initialize the params-->
+
   Username = document.getElementById('usernameinput').value;
   Password = document.getElementById('passwordinput').value;
   Server = document.getElementById('serverinput').value;
 
-  <!--Build the form data-->
+
   let formData = new FormData();
   formData.append('client_id', 'unity.client');
   formData.append('client_secret', 'secret');
@@ -18,14 +18,14 @@ function Login1() {
   formData.append('password', Password);
   formData.append('acr_values', 'gameId:j68d');
 
-  <!--Getting the url-->
+
   let url = "https://eu.mspapis.com/loginidentity/connect/token";
 
   if(Server === 'us' || Server === 'UnitedStates') {
     url = "https://us.mspapis.com/loginidentity/connect/token";
   }
 
-  <!--Send first login request to api-->
+
   fetch(url,
   {
         body: formData,
